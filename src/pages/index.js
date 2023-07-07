5d6d679c391fffbc8a5d644d6acdbbac3c82bbf5
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-07-08 00:42:18 +0700

    code(v1): make test for vercel update

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
             w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center"
             >
               <AnimatedText
-                text="Turning Vision Into Reality With Code And Design"
+                text="Axel Vision Into Reality With Code And Design"
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
